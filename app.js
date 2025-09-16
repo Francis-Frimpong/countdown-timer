d2bbf7136bbe556d2b95c25a47b52e47b01468c1
Francis <brakay1136@gmail.com>
2025-09-16 09:11:30 -0700

    project done

--- app.js
+++ app.js
@@ -19,8 +19,45 @@ class CountDownTimer {
   }
 
   countDown() {
-    this.timer = setInterval(() => {});
+    this.timer = setInterval(() => {
+      if (
+        this.day == 0 &&
+        this.hour == 0 &&
+        this.minute == 0 &&
+        this.second == 0
+      ) {
+        clearInterval(this.timer);
+        return;
+      }
+
+      if (this.second > 0) {
+        this.second--;
+      } else {
+        if (this.minute > 0) {
+          this.minute--;
+          this.second = 59;
+        } else if (this.hour > 0) {
+          this.hour--;
+          this.minute = 59;
+          this.second = 59;
+        } else if (this.day > 0) {
+          this.day--;
+          this.hour = 23;
+          this.minute = 59;
+          this.second = 59;
+        }
+      }
+
+      this.days.textContent = this.day < 10 ? "0" + this.day : this.day;
+      this.hours.textContent = this.hour < 10 ? "0" + this.hour : this.hour;
+      this.minutes.textContent =
+        this.minute < 10 ? "0" + this.minute : this.minute;
+      this.seconds.textContent =
+        this.second < 10 ? "0" + this.second : this.second;
+    }, 1000);
+    // update UI
   }
 }
 
 const timer = new CountDownTimer();
+timer.countDown();
